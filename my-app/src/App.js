282ad65f0895df7543388dd3f2f7d2842593da11
ZNevzz <dsouza.nevil45@gmail.com>
2020-04-26 11:46:32 +0530

    app started, router-link not working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from "react-router-dom";
+
+import { HeatMap } from './heatmap/HeatMap.js'
+
+function Home() {
+    return (
+
+               <div className="App">
+                 <header className="App-header">
+                   <img src={logo} className="App-logo" alt="logo" />
+                   <p>
+                     Highcharts Demo
+                   </p>
+                   <a
+                     className="App-link"
+                     href="https://reactjs.org"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                   >
+                     Learn React
+                   </a>
+                 </header>
+                 <main>
+
+                   <Link to="/heatmap">HeatMap</Link>
+
+                 </main>
+
+                 <div>
+                   <Route path="/heatmap" component={HeatMap} />
+                 </div>
+               </div>
+             );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Home />
+
+
   );
 }
 
---
+++ my-app/src/heatmap/HeatMap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function HeatMap() {
+    return (
+    <>
+        <h1>HeatMap using Highcharts</h1>
+        /* TODO: className */
+        <div id="heatmap-container-1" >
+        </div>
+    </>
+    );
+}
